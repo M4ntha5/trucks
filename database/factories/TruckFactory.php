183792f0253c733922d71999d621cccf3cb32b12
M4ntha5 <mantas.daunoravicius@ktu.edu>
2020-05-28 21:49:52 +0300

    migrations update

--- database/factories/TruckFactory.php
+++ database/factories/TruckFactory.php
@@ -2,15 +2,15 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Truck;
+use App\Models\Truck;
 use Faker\Generator as Faker;
 
 $factory->define(Truck::class, function (Faker $faker) {
     return [
-        'brand' => $faker->randomDigit(),
+        'brand' => $faker->numberBetween(1,4),
         'year_made' => $faker->numberBetween(1900, 2020),
-        'user' => $faker->name(),
-        'users_count' => $faker->randomDigit(),
-        'comment' => $faker->sentences()
+        'user' => $faker->name,
+        'users_count' => $faker->randomDigit,
+        'comment' => $faker->sentence()
     ];
 });
--- database/seeds/TrucksTableSeeder.php
+++ database/seeds/TrucksTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Truck;
+use App\Models\Truck;
 
 class TrucksTableSeeder extends Seeder
 {
